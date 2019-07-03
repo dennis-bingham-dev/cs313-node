ed65c5575961089c4b6c9edc02225936015a7ad8
Bingham-Dennis <bin12011@byui.edu>
2019-07-03 14:25:11 -0600

    added views

--- server.js
+++ server.js
@@ -1,18 +1,29 @@
 const express = require('express');
-var router = express.Router();
 const app = express();
+app.set('view engine', 'ejs');
 let port = process.env.PORT;
 if (port === null || port === "" || port === undefined) {
   port = 8000;
 }
 
+app.get('/', (req, res) => {
+  res.render('pages/index');
+});
+
+app.get('/signup', (req, res) => {
+  res.render('pages/signup');
+});
+
+app.get('/login', (req, res) => {
+  res.render('pages/login');
+});
+
+app.get('/forgotpassword', (req, res) => {
+  res.render('pages/forgotpassword');
+});
+
 app.listen(port);
 console.log(`Server listening on port ${port}...`);
-
-app.get('/', (req, res) => {
-  res.write('Hello World');
-  res.end();
-});
 
 setInterval(() => {
   console.log(`Server listening on port ${port}...`);
