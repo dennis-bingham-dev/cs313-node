0d8e550013c9d79ddba3a121d97ff9a8073fdfd8
Bingham-Dennis <bin12011@byui.edu>
2019-07-03 14:06:12 -0600

    update files

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require('express');
+var router = express.Router();
 const app = express();
-const port = process.env.PORT;
-if (port === null || port === "") {
+let port = process.env.PORT;
+if (port === null || port === "" || port === undefined) {
   port = 8000;
 }
 
 app.listen(port);
+console.log(`Server listening on port ${port}...`);
+
+app.get('/', (req, res) => {
+  res.write('Hello World');
+  res.end();
+});
+
+setInterval(() => {
+  console.log(`Server listening on port ${port}...`);
+}, 10000);
